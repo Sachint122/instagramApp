45019770c00f395f44380479671cbc54d57a9b28
Sachin22 <sachintiwari.751858@gmail.com>
2025-06-21 23:41:11 +0530

    final commit

--- src/reelsData.js
+++ src/reelsData.js
@@ -1,20 +1,23 @@
 
 import profie from './assest/profile.jpg'
 import profie2 from './assest/profile2.jpg'
+import video1 from './assest/video.mp4'
+
+import video2 from './assest/instagram_1740453614003(720p).mp4'
 const reelsData = [
     {
       id: 1,
-      videoUrl: "https://www.w3schools.com/html/mov_bbb.mp4",
+      videoUrl: video1,
       username: "sachin_tiwari",
       profilePic: profie,
-      caption: "Exploring the hills 🌄 #travel #reels",
+      caption: "Broken gamer and Developer #gaming #reels",
       likes: 12200,
       isLiked: false,
       comments: 89,
     },
     {
       id: 2,
-      videoUrl: "https://www.w3schools.com/html/movie.mp4",
+      videoUrl: video2,
       username: "devil.codes",
       profilePic: profie2,
       caption: "Coding late nights 💻🔥",
